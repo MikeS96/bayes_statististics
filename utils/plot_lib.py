1f1313980c3cda0ad9639a177eb3bcf071dea935
MikeS96 <miguel.saaruiz@gmail.com>
2021-04-23 19:53:35 -0500

    Added plotting file to set plots' style

---
+++ utils/plot_lib.py
@@ -0,0 +1,7 @@
+from matplotlib import pyplot as plt
+
+def set_default(figsize=(10, 10), dpi=100):
+    plt.style.use(['dark_background', 'bmh'])
+    plt.rc('axes', facecolor='k')
+    plt.rc('figure', facecolor='k')
+    plt.rc('figure', figsize=figsize, dpi=dpi)
